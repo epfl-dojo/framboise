c5294f48e05e18755526e99eda94556278c6070b
Nicolas Crausaz <nicrausaz@users.noreply.github.com>
2016-05-09 16:03:50 +0200

    Create button.py

---
+++ button.py
@@ -0,0 +1,20 @@
+#print in terminal when button is pressed
+
+import RPi.GPIO as GPIO
+import time
+
+buttonPin = 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+input = GPIO.input(buttonPin)
+
+while True:
+    
+    input_state = GPIO.input(buttonPin)
+    
+    if input_state == False: 
+        print("Button Pressed")
+        time.sleep(0.5)
+
