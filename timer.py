3a9b07b4e3cac3e042a46641a6a86f8dbcdc29e2
Loïc Humbert <loic.humbert@epfl.ch>
2016-05-27 16:48:05 +0200

    added timer.py which works but needs button pressure

---
+++ timer.py
@@ -0,0 +1,95 @@
+import pygame
+import RPi.GPIO as GPIO
+import time
+
+def test():
+    #print when button is pressed
+    buttonPin = 21
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    input = GPIO.input(buttonPin)
+
+    while True:
+    
+        input_state = GPIO.input(buttonPin)
+    
+        if input_state == False: 
+            timer();
+            time.sleep(0.5)
+    return
+
+def timer():
+    
+        # Define some colors
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+         
+        pygame.init()
+         
+        # Set the height and width of the screen
+        size = [300, 200]
+        screen = pygame.display.set_mode(size)
+         
+        pygame.display.set_caption("Timer")
+         
+        # Loop until the user clicks the close button.
+        done = False
+         
+        # Used to manage how fast the screen updates
+        clock = pygame.time.Clock()
+         
+        font = pygame.font.Font(None, 50)
+         
+        frame_count = 0
+        frame_rate = 60
+        start_time = 300
+         
+        # -------- Main Program Loop -----------
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+         
+            # Set the screen background
+            screen.fill(BLACK)
+         
+            # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+         
+            # Calculate total seconds
+            total_seconds = start_time - (frame_count // frame_rate)
+            if total_seconds < 0:
+                total_seconds = 0
+         
+            # Divide by 60 to get total minutes
+            minutes = total_seconds // 60
+         
+            # Use modulus (remainder) to get seconds
+            seconds = total_seconds % 60
+         
+            # Use python string formatting to format in leading zeros
+            output_string = "{0:02}:{1:02}".format(minutes, seconds)
+         
+            # Blit to the screen
+            text = font.render(output_string, True, WHITE)
+         
+            screen.blit(text, [100, 100])
+         
+            # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+            frame_count += 1
+         
+            # Limit frames per second
+            clock.tick(frame_rate)
+         
+            # Go ahead and update the screen with what we've drawn.
+            pygame.display.flip()
+         
+        # Be IDLE friendly. If you forget this line, the program will 'hang'
+        # on exit.
+        pygame.quit()
+        
+    	return
+
+test();
+
